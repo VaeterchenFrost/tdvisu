a8d0d47a98b8856432da41c0223733d83dc5d37e
Martin Röbke <martin.roebke@web.de>
2020-06-01 22:53:52 +0200

    Update setup.py
    Replaced io.open with open removing one import
    Adding delimiter argument making it more usable.
    Adding Docstring
    #5

--- setup.py
+++ setup.py
@@ -1,14 +1,34 @@
 # -*- coding: utf-8 -*-
-import io
+"""Describing local installs or distribution of the package tdvisu."""
+
 from setuptools import setup
 from tdvisu.version import __version__ as version
 
-def read_files(files):
+
+def read_files(files, delim: str = "\n") -> str:
+    r"""
+    Concatenate the content of one or more files joined by a delimiter.
+
+    Parameters
+    ----------
+    files : Iterable of single files
+        Every file is a text or byte string giving the name
+        (and the path if the file isn't in the current working directory)
+         of the file to be opened or an integer file descriptor
+         of the file to be wrapped.
+    delim : str, optional
+        The delimiter to be inserted between the contents. The default is "\n".
+
+    Returns
+    -------
+    str
+        The concatenated string.
+    """
     data = []
     for file in files:
-        with io.open(file, encoding='utf-8') as f:
-            data.append(f.read())
-    return "\n".join(data)
+        with open(file, encoding='utf-8') as handle:
+            data.append(handle.read())
+    return delim.join(data)
 
 
 long_description = read_files(['README.md', 'CHANGELOG.md'])
