f21f2000594ed3ba20344441374e1991babb2701
Martin Röbke <martin.roebke@web.de>
2020-06-02 01:24:48 +0200

    Update version.py
    Next version is -dev.

--- tdvisu/version.py
+++ tdvisu/version.py
@@ -1,10 +1,10 @@
 """
 Release versions have the form "M.N.P".
-Dev versions have the form "M.N.P+dev" (PLUS sign to conform to PEP 440).
+Dev versions have the form "M.N.P-devN".
 """
 
 # Base version.
-__version__ = '0.5.1'
+__version__ = '0.5.1-dev'
 
 # Year-Month-Day of last version change
 __date__ = '2020-06-01'
