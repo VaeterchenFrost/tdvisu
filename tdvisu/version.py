6e6b4a589822a0594459bf30de45ca3c1dc8e63a
Martin Röbke <martin.roebke@web.de>
2020-06-01 20:51:07 +0200

    Update version.py
    For pypi #4

--- tdvisu/version.py
+++ tdvisu/version.py
@@ -4,7 +4,7 @@ Dev versions have the form "M.N.P+dev" (PLUS sign to conform to PEP 440).
 """
 
 # Base version.
-__version__ = '0.5.0.dev2'
+__version__ = '0.5.0.dev3'
 
 # Year-Month-Day of last version change
 __date__ = '2020-06-01'
