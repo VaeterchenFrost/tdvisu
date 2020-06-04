f93f6aa7f2c1ebf5ebbeb4d46f30bde2f4e33152
Martin Röbke <martin.roebke@web.de>
2020-06-04 15:02:59 +0200

    Bump version
    removed __apiversion__ (not needed)

--- tdvisu/version.py
+++ tdvisu/version.py
@@ -4,10 +4,7 @@ Dev versions have the form "M.N.P-devN".
 """
 
 # Base version.
-__version__ = '0.5.1-dev'
+__version__ = '1.0.0'
 
 # Year-Month-Day of last version change
-__date__ = '2020-06-01'
-
-# Supported version of the JsonAPI
-__apiversion__ = '1.3'
+__date__ = '2020-06-04'
