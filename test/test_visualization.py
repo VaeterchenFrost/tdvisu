9401dfdbdc0ffbcd77c8268909e84f2b5d84c789
Martin Röbke <martin.roebke@web.de>
2020-06-26 00:53:25 +0200

    Create test_visualization
    Create test_visualization with
    test\dbjson4andjoin.json
    expected images each 1,2,6
    writing to pytest tempdir

---
+++ test/test_visualization.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+Testing visualization.py
+
+
+Copyright (C) 2020  Martin Röbke
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.
+    If not, see https://www.gnu.org/licenses/gpl-3.0.html
+
+"""
+
+import argparse
+from pathlib import Path
+
+from tdvisu.visualization import main
+
+
+def test_sat_and_join(tmpdir):
+    """Complete visualization run with svgjoin."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('infile',
+                        type=argparse.FileType('r', encoding='UTF-8'),
+                        help="Input file for the visualization "
+                        "must conform with the 'JsonAPI.md'")
+    parser.add_argument('outfolder',
+                        help="Folder to output the visualization results")
+    parser.add_argument('--loglevel', help="set the minimal loglevel for root")
+
+    # get cmd-arguments
+    infile = Path(__file__).parent / 'dbjson4andjoin.json'
+    outfolder = Path(tmpdir) / 'temp-test_sat_and_join'
+    _args = parser.parse_args([str(infile), str(outfolder)])
+    # call main()
+    main(_args)
+    files = [file for file in outfolder.iterdir() if file.is_file()]
+    assert len(files
+               ) == 42, "total files"
+    assert len([file for file in files if file.suffix == '.svg']
+               ) == 24, "svg files"
+    assert len([file for file in files if file.suffix == '']
+               ) == 18, "dot source files"
+
+    testobjects = [
+        "combined1.svg",
+        "combined2.svg",
+        "combined6.svg",
+        "IncidenceGraphStep1.svg",
+        "IncidenceGraphStep2.svg",
+        "IncidenceGraphStep6.svg",
+        "PrimalGraphStep6.svg",
+        "PrimalGraphStep1.svg",
+        "PrimalGraphStep2.svg",
+        "TDStep1.svg",
+        "TDStep2.svg",
+        "TDStep6.svg"]
+
+    expected = [Path(__file__).parent / 'expected_images' / file
+                for file in testobjects]
+
+    for file in testobjects:
+        with open(Path(__file__).parent / 'expected_images' / file) as expected:
+            with open(outfolder / file) as result:
+                assert result.read() == expected.read(
+                ), f"{file} should be the same"
