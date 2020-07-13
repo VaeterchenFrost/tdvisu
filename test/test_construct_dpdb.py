ab44a2dcee9a3e9be8726ba4b2f32b33c510cc60
Martin Röbke <martin.roebke@web.de>
2020-07-13 22:52:48 +0200

    Complete test_main run
    Output in expected_images

--- test/test_construct_dpdb.py
+++ test/test_construct_dpdb.py
@@ -22,8 +22,10 @@ Copyright (C) 2020  Martin Röbke
 """
 import argparse
 import datetime
-import psycopg2 as pg
+import tempfile
 from pathlib import Path
+import psycopg2 as pg
+
 from tdvisu.construct_dpdb_visu import (read_cfg, db_config, DEFAULT_DBCONFIG,
                                         IDpdbVisuConstruct, DpdbSharpSatVisu,
                                         DpdbSatVisu, DpdbMinVcVisu, main)
@@ -79,44 +81,78 @@ def test_problem_interface():
 
 def test_main(mocker):
     """Test behaviour of construct_dpdb_visu.main"""
-    PARSER = argparse.ArgumentParser()
 
-    PARSER.add_argument('problemnumber', type=int,
+    mock_connect = mocker.patch('tdvisu.construct_dpdb_visu.pg.connect')
+    ta_status = mock_connect.return_value.__enter__.return_value.get_transaction_status
+    ta_status.return_value = pg.extensions.TRANSACTION_STATUS_IDLE
+
+    mocker.patch(
+        'tdvisu.construct_dpdb_visu.query_problem',
+        return_value=('Sat',))
+    mocker.patch('tdvisu.construct_dpdb_visu.query_num_vars',
+                 return_value=8)
+    mocker.patch(
+        'tdvisu.construct_dpdb_visu.query_td_node_status_ordered',
+        return_value=[(3,), (5,), (4,), (2,), (1,)])
+    mocker.patch(
+        'tdvisu.construct_dpdb_visu.query_sat_clause',
+        return_value=[(True, None, None, True, None, True, None, None, None, None),
+                      (True, None, None, None, False,
+                       None, None, None, None, None),
+                      (False, None, None, None, None,
+                       None, True, None, None, None),
+                      (None, True, True, None, None, None, None, None, None, None),
+                      (None, True, None, None, True, None, None, None, None, None),
+                      (None, True, None, None, None,
+                       False, None, None, None, None),
+                      (None, None, True, None, None,
+                       None, None, False, None, None),
+                      (None, None, None, True, None,
+                       None, None, False, None, None),
+                      (None, None, None, False, None,
+                       True, None, None, None, None),
+                      (None, None, None, False, None, None, True, None, None, None)])
+
+    mocker.patch('tdvisu.construct_dpdb_visu.query_td_bag_grouped',
+                 return_value=[[1, 2, 3, 4, 5]])
+    mocker.patch('tdvisu.construct_dpdb_visu.query_td_node_status', return_value=(
+        "2020-07-13 02:06:18.053880", datetime.timedelta(microseconds=768)))
+    mocker.patch('tdvisu.construct_dpdb_visu.query_td_bag',
+                 return_value=[(1,), (2,), (4,), (6,)])
+    mocker.patch('tdvisu.construct_dpdb_visu.query_column_name',
+                 return_value=[('v1',), ('v2',), ('v4',), ('v6',)])
+    mocker.patch(
+        'tdvisu.construct_dpdb_visu.query_bag',
+        return_value=[(False, None, False, None),
+                      (True, None, True, None),
+                      (False, None, True, None),
+                      (True, None, False, None)])
+
+    mocker.patch('tdvisu.construct_dpdb_visu.query_edgearray',
+                 return_value=[(2, 1), (3, 2), (4, 2), (5, 4)])
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('problemnumber', type=int,
                         help="selected problem-id in the postgres-database.")
-    PARSER.add_argument('--twfile',
+    parser.add_argument('--twfile',
                         type=argparse.FileType('r', encoding='UTF-8'),
                         help="tw-file containing the edges of the graph - "
                         "obtained from dpdb with option --gr-file GR_FILE.")
-    PARSER.add_argument('--loglevel', help="set the minimal loglevel for root")
-    PARSER.add_argument('--outfile', default='dbjson%d.json',
+    parser.add_argument('--loglevel', help="set the minimal loglevel for root")
+    parser.add_argument('--outfile', default='dbjson%d.json',
                         help="default:'dbjson%%d.json'")
-    PARSER.add_argument('--pretty', action='store_true',
+    parser.add_argument('--pretty', action='store_true',
                         help="pretty-print the JSON.")
-    PARSER.add_argument('--inter-nodes', action='store_true',
+    parser.add_argument('--inter-nodes', action='store_true',
                         help="calculate and animate the shortest path between "
                         "successive bags in the order of evaluation.")
-    # get cmd-arguments
-    _args = PARSER.parse_args(['1'])
-    
-    mock_connect = mocker.patch('tdvisu.construct_dpdb_visu.pg.connect')
-    mock_connect.return_value.__enter__.return_value.get_transaction_status.return_value = pg.extensions.TRANSACTION_STATUS_IDLE
-    
-    mocker.patch('tdvisu.construct_dpdb_visu.query_problem', return_value=('Sat',))
-    mocker.patch('tdvisu.construct_dpdb_visu.query_num_vars', return_value=5)
-    mocker.patch('tdvisu.construct_dpdb_visu.query_td_node_status_ordered', return_value=[[1,2],[3,4]])
-    mocker.patch('tdvisu.construct_dpdb_visu.query_sat_clause', return_value=[(True, None, None, True, None, True, None, None, None, None), (True, None, None, None, False, None, None, None, None, None), (False, None, None, None, None, None, True, None, None, None), (None, True, True, None, None, None, None, None, None, None), (None, True, None, None, True, None, None, None, None, None), (None, True, None, None, None, False, None, None, None, None), (None, None, True, None, None, None, None, False, None, None), (None, None, None, True, None, None, None, False, None, None), (None, None, None, False, None, True, None, None, None, None), (None, None, None, False, None, None, True, None, None, None)])
-    mocker.patch('tdvisu.construct_dpdb_visu.query_td_bag_grouped', return_value=[[1, 2, 3, 4, 5]])
-    mocker.patch('tdvisu.construct_dpdb_visu.query_td_node_status', return_value=("2020-07-13 02:06:18.053880", datetime.timedelta(microseconds=768)))
-    mocker.patch('tdvisu.construct_dpdb_visu.query_td_bag', return_value=[[5,6,7]])
-    mocker.patch('tdvisu.construct_dpdb_visu.query_td_node_status_ordered', return_value=[[4,2,3,1]])
-    mocker.patch('tdvisu.construct_dpdb_visu.query_column_name', return_value=[["column_name"]])
-    mocker.patch('tdvisu.construct_dpdb_visu.query_bag', return_value=[["1","2","4","3"]])
-    mocker.patch('tdvisu.construct_dpdb_visu.query_edgearray', return_value=[(2, 1), (3, 2), (4, 2), (5, 4)])
-  
-    try:
+
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        # set cmd-arguments
+        outfile = str(Path(tmpdirname) / 'test_main.json')
+        _args = parser.parse_args(['1', '--outfile', outfile])
+        # one mocked run
         main(_args)
-    except ValueError as err:
-        print(err)
-        raise
+
     # assert mock_con_cm.assert_called()
     # assert mock_con_cm.assert_called()
