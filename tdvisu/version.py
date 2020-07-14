c9dbae256ee32c0fc996e1e31a14c7d804715e8a
Martin Röbke <martin.roebke@web.de>
2020-07-14 02:43:06 +0200

    version 1.1.4

--- tdvisu/version.py
+++ tdvisu/version.py
@@ -4,7 +4,7 @@ Dev versions have the form "M.N.P-devN".
 """
 
 # Base version.
-__version__ = '1.1.3'
+__version__ = '1.1.4'
 
 # Year-Month-Day of last version change
-__date__ = '2020-07-09'
+__date__ = '2020-07-14'
