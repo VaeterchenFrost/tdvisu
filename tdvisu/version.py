b6171935795fff0207eba24cca4177569a3d7476
Martin Röbke <martin.roebke@web.de>
2020-09-13 23:54:56 +0200

    Update version.py
    Version bump to 1.1.7

--- tdvisu/version.py
+++ tdvisu/version.py
@@ -4,7 +4,7 @@ Dev versions have the form "M.N.P-devN".
 """
 
 # Base version.
-__version__ = '1.1.6'
+__version__ = '1.1.7'
 
 # Year-Month-Day of last version change
-__date__ = '2020-08-01'
+__date__ = '2020-09-13'
