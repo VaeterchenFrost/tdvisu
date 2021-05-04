3adb2594ed0a5538b2b6a529c3b2ef109e3746d3
Martin Röbke <martin.roebke@web.de>
2021-05-04 20:13:14 +0200

    Update version.py
    to version 1.1.8

--- tdvisu/version.py
+++ tdvisu/version.py
@@ -4,7 +4,7 @@ Dev versions have the form "M.N.P-devN".
 """
 
 # Base version.
-__version__ = '1.1.7'
+__version__ = '1.1.8'
 
 # Year-Month-Day of last version change
-__date__ = '2020-09-13'
+__date__ = '2021-05-04'
